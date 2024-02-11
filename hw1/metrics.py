46b19835e79ac2608166b51131de8bcc4ee7e18c
Greenberg Michael <greenerkk@gmail.com>
2024-02-11 12:33:45 +0300

    Add metrics for classifier

--- hw1/metrics.py
+++ hw1/metrics.py
@@ -16,10 +16,45 @@ def binary_classification_metrics(y_pred, y_true):
     # https://en.wikipedia.org/wiki/Precision_and_recall
     # https://en.wikipedia.org/wiki/F1_score
 
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+    int_y_pred = y_pred.astype(int)
+    int_y_true = y_true.astype(int)
+
+    true_positive = 0
+    true_negative = 0
+    false_positive = 0
+    false_negative = 0
+
+    for idx in range(y_true.shape[0]):
+        if int_y_true[idx] == 1 and int_y_pred[idx] == 1:
+            true_positive += 1
+        if int_y_true[idx] == 0 and int_y_pred[idx] == 0:
+            true_negative += 1
+        if int_y_true[idx] == 0 and int_y_pred[idx] == 1:
+            false_positive += 1
+        if int_y_true[idx] == 1 and int_y_pred[idx] == 0:
+            false_negative += 1
+
+    if (true_positive + true_negative + false_positive + false_negative) != 0:
+        accuracy = (true_positive + true_negative) / (true_positive + true_negative + false_positive + false_negative)
+    else:
+        accuracy = None
+
+    if (true_positive + false_positive) != 0:
+        precision = true_positive / (true_positive + false_positive)
+    else:
+        precision = None
+
+    if (true_positive + false_negative) != 0:
+        recall = true_positive / (true_positive + false_negative)
+    else:
+        recall = None
+
+    if (precision or recall) is not None:
+        f1 = 2 * precision * recall / (precision + recall)
+    else:
+        f1 = None
+
+    return precision, recall, f1, accuracy
 
 
 def multiclass_accuracy(y_pred, y_true):
@@ -31,11 +66,14 @@ def multiclass_accuracy(y_pred, y_true):
     Returns:
     accuracy - ratio of accurate predictions to total samples
     """
+    positive_results = 0
 
-    """
-    YOUR CODE IS HERE
-    """
-    pass
+    for idx in range(y_true.shape[0]):
+        if y_pred[idx] == y_true[idx]:
+            positive_results += 1
+
+    accuracy = positive_results/y_true.shape[0]
+    return accuracy
 
 
 def r_squared(y_pred, y_true):
